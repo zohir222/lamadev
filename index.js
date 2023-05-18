1dd0912a8b8ab7c1eb406539b568d7b0539b6bb8
zohir222 <bensalemzohir284@gmail.com>
2023-05-18 12:29:02 +0100

    koko

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ const userRoute = require('./routes/users') ;
 
 app.use("/user" , userRoute ) ;
 
-app.listen( process.env.PORT , ()=> {
-    console.log("**********server runningn port 3010*************");
+app.listen( process.env.PORT || 3010 , ()=> {
+    console.log(`**********server runningn port 3010  *************`);
 } )
